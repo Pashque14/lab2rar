c3efdc12a0fed83fbfc6277dce4a4816446d547d
pashque14 <pashque14@gmail.com>
2024-12-19 12:57:07 +0500

    commit3

--- main.cpp
+++ main.cpp
@@ -28,30 +28,9 @@ struct fileHead{
     uint16_t nameSize;
     uint32_t fileAttr;
 };
-
 #pragma pack(pop)
 void readarc(vector<char> x){
-    int index = 14 + 6;
-    while(true){
-        head * p_header = reinterpret_cast<head*> (&x[index]);
-        index += 7;
-        if(int(p_header -> header_type) != 116){ // 74 из 16-й в 10-ю
-            break;
-        }
-        else{
-            fileHead* f_header = reinterpret_cast<fileHead*> (&x[index]);
-            index += 25;
-                for (int i = index; i< index + int(f_header -> nameSize); i++){
-                    cout << x[i];
-                }
-                cout << endl;
-                index -= 25;
-                index -= 7;
-                index += int(f_header -> packSize);
-                index += p_header -> header_size;
-            }
-            cout << "\n" << endl;
-        }
+
 }
 int main()
 {
@@ -64,5 +43,6 @@ int main()
     file.seekg(0, ios::beg);
     vector<char> arr(size,0);
     file.read(arr.data(), size);
+    readarc(arr);
 
 }
