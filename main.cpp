a9df4c0518cec03cdc0995b39f70746486796154
pashque14 <pashque14@gmail.com>
2024-12-19 12:58:13 +0500

    commit4

--- main.cpp
+++ main.cpp
@@ -30,7 +30,27 @@ struct fileHead{
 };
 #pragma pack(pop)
 void readarc(vector<char> x){
-
+    int index = 14 + 6;
+    while(true){
+        head * p_header = reinterpret_cast<head*> (&x[index]);
+        index += 7;
+        if(int(p_header -> header_type) != 116){
+            break;
+        }
+        else{
+            fileHead* f_header = reinterpret_cast<fileHead*> (&x[index]);
+            index += 25;
+                for (int i = index; i< index + int(f_header -> nameSize); i++){
+                    cout << x[i];
+                }
+                cout << endl;
+                index -= 25;
+                index -= 7;
+                index += int(f_header -> packSize);
+                index += p_header -> header_size;
+            }
+            cout << "\n" << endl;
+        }
 }
 int main()
 {
